be449d295e7bba5c43d492e68b502f212816295a
Robert Moore <me@robdmoore.id.au>
2013-08-10 23:56:30 +0800

    Initial code and tests

---
+++ WindowsAzure.Storage.Services.Tests/AzureStorageFactoryTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using NUnit.Framework;
+
+namespace WindowsAzure.Storage.Services.Tests
+{
+    class AzureStorageFactoryTests
+    {
+        private const string Name = "azurestoragefactorytests";
+        private static readonly CloudStorageAccount StorageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+
+        private CloudQueue _queue;
+        private CloudBlobContainer _blob;
+        private CloudTable _table;
+
+        [SetUp]
+        public void Setup()
+        {
+            _queue = StorageAccount.CreateCloudQueueClient().GetQueueReference(Name);
+            _blob = StorageAccount.CreateCloudBlobClient().GetContainerReference(Name);
+            _table = StorageAccount.CreateCloudTableClient().GetTableReference(Name);
+
+            _queue.DeleteIfExists();
+            _blob.DeleteIfExists();
+            _table.DeleteIfExists();
+        }
+
+        [Test]
+        public void GivenStorageFactoryConstructedWithStorageAccount_WhenGettingBlobContainer_ThenReturnCorrectBlobContainer()
+        {
+            var factory = new AzureStorageFactory(StorageAccount);
+
+            var blobContainer = factory.GetBlobContainer(Name);
+
+            Assert.That(blobContainer.Exists(), Is.False);
+            _blob.CreateIfNotExists();
+            Assert.That(blobContainer.Exists(), Is.True);
+        }
+
+        [Test]
+        public void GivenStorageFactoryConstructedWithStorageAccount_WhenGettingQueue_ThenReturnCorrectQueue()
+        {
+            var factory = new AzureStorageFactory(StorageAccount);
+
+            var queue = factory.GetQueue(Name);
+
+            Assert.That(queue.Exists(), Is.False);
+            _queue.CreateIfNotExists();
+            Assert.That(queue.Exists(), Is.True);
+        }
+
+        [Test]
+        public void GivenStorageFactoryConstructedWithStorageAccount_WhenGettingTable_ThenReturnCorrectTable()
+        {
+            var factory = new AzureStorageFactory(StorageAccount);
+
+            var table = factory.GetTable(Name);
+
+            Assert.That(table.Exists(), Is.False);
+            _table.CreateIfNotExists();
+            Assert.That(table.Exists(), Is.True);
+        }
+    }
+}
---
+++ WindowsAzure.Storage.Services/AzureStorageFactory.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WindowsAzure.Storage.Services
+{
+    /// <summary>
+    /// Factory that returns Azure Storage services for a storage account.
+    /// </summary>
+    public interface IAzureStorageFactory
+    {
+        /// <summary>
+        /// Get a raw blob container.
+        /// </summary>
+        /// <param name="containerName">The name of the container to retrieve</param>
+        /// <returns>The blob container for the specified container name</returns>
+        CloudBlobContainer GetBlobContainer(string containerName);
+
+        /// <summary>
+        /// Get a raw queue.
+        /// </summary>
+        /// <param name="queueName">The name of the queue to retrieve</param>
+        /// <returns>The queue of the specified name</returns>
+        CloudQueue GetQueue(string queueName);
+
+        /// <summary>
+        /// Get a raw table.
+        /// </summary>
+        /// <param name="tableName">The name of the table to retrieve</param>
+        /// <returns>The table of the specified name</returns>
+        CloudTable GetTable(string tableName);
+    }
+
+    /// <summary>
+    /// Factory that returns Azure Storage services for a storage account.
+    /// </summary>
+    public class AzureStorageFactory : IAzureStorageFactory
+    {
+        private readonly CloudStorageAccount _storageAccount;
+
+        /// <summary>
+        /// Construct an Azure Storage Factory with a connection string.
+        /// </summary>
+        /// <param name="connectionString">The connection string for the Azure Storage account to provide services for</param>
+        public AzureStorageFactory(string connectionString)
+        {
+            _storageAccount = CloudStorageAccount.Parse(connectionString);
+        }
+
+        /// <summary>
+        /// Construct an Azure Storage Factory with storage account details.
+        /// </summary>
+        /// <param name="storageAccount">The storage account details for the Azure Storage account to provide services for</param>
+        public AzureStorageFactory(CloudStorageAccount storageAccount)
+        {
+            _storageAccount = storageAccount;
+        }
+
+        public CloudBlobContainer GetBlobContainer(string containerName)
+        {
+            var blobClient = _storageAccount.CreateCloudBlobClient();
+            return blobClient.GetContainerReference(containerName);
+        }
+
+        public CloudQueue GetQueue(string queueName)
+        {
+            var queue = _storageAccount.CreateCloudQueueClient();
+            return queue.GetQueueReference(queueName);
+        }
+
+        public CloudTable GetTable(string tableName)
+        {
+            var queue = _storageAccount.CreateCloudTableClient();
+            return queue.GetTableReference(tableName);
+        }
+    }
+}
